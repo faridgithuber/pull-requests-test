6baefcdaf74d154fb80182dc07f95a42d537b3c9
faridhaghgooyan <faridhaghgooyan@gmail.com>
2023-02-11 14:08:07 +0100

    height and weight properties was added to parent Class

--- dev1/class.php
+++ dev1/class.php
@@ -1,7 +1,12 @@
 <?php
 
 class User{
-   
+    protected $height;
+    protected $weight;
+    public function __construct($height,$weight){
+        $this->height = $height;
+        $this->weight = $weight;
+    }
 }
 
 class Author extends User{
